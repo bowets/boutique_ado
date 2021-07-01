1606a62c57d7a5944ef8f140ab9da949faafa448
bforsek <bforsek@gmail.com>
2021-07-01 21:33:28 +0100

    Adding stripe card

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'strip_public_key': 'pk_test_51J8TH6JCvONQIszN8CmraPKb2feb0d8dbpiyyQVp04gdk4JAfOOobiXanmWd8o09EJV12hocqJbYhyJuzC43d3LC00VARaiq8D',
+        'client_secret': 'sk_test_51J8TH6JCvONQIszNgQipsLbF2s8odxlRX0VyxG02nyVi2WvJyF0m3S1KaX6f6Nj6dXQaeoL4jN5a8WKkUyEMnKZ700GUbsnZrA'
     }
 
     return render(request, template, context)
