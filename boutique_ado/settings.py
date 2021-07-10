0a52b5793c0bb5180e850f3917377eea841b1c23
bforsek <bforsek@gmail.com>
2021-07-10 22:27:44 +0100

    Change secret key to environ variable

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-16fkm_fg1&518ry103obct-f4-qvdu+gu!t!uturu*yde%cn(#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bowets-moutique-ado.herokuapp.com', 'localhost']
 
